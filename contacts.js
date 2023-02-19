7c67a5112ccd7ccf9cb0201955f3a2a2dae6aa84
Mike Chernetskyi <michael.chernetskyi@gmail.com>
2023-02-19 13:47:20 +0200

    update contact js

--- contacts.js
+++ contacts.js
@@ -5,13 +5,9 @@ const contactsPath = path.resolve("db/contact.json");
 const { v4: uuidv4 } = require("uuid");
 
 async function listContacts() {
-  try {
-    const data = await fs.readFile(contactsPath);
-    console.table(JSON.parse(data));
-  } catch (err) {
-    console.log(err.message);
-    throw err;
-  }
+  fs.readFile(contactsPath)
+    .then((data) => console.table(JSON.parse(data)))
+    .catch((err) => console.log(err.message));
 }
 
 async function getContactById(contactId) {
@@ -32,22 +28,20 @@ async function getContactById(contactId) {
 
 async function removeContact(contactId) {
   try {
-    const data = await fs.readFile(contactsPath);
-    const parsedData = JSON.parse(data);
-    const result = parsedData.find(
+    const contacts = JSON.parse(await fs.readFile(contactsPath));
+    const contactIndex = contacts.findIndex(
       (contact) => contact.id.toString() === contactId.toString()
     );
-    if (!result) {
+    if (contactIndex === -1) {
       console.log("Contact not found");
       return;
     }
-    const filteredData = parsedData.filter(
-      (contact) => contact.id.toString() !== contactId.toString()
-    );
-    await fs.writeFile(contactsPath, JSON.stringify(filteredData));
+    contacts.splice(contactIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
     console.log(`Contact with ID ${contactId} removed`);
-  } catch (err) {
-    console.log(err.message);
+  } catch (error) {
+    console.error(error.message);
+    throw error;
   }
 }
 
@@ -55,11 +49,7 @@ async function addContact(name, email, phone) {
   try {
     const data = await fs.readFile(contactsPath);
     const parsedData = JSON.parse(data);
-    if (
-      parsedData.find(
-        (contact) => contact.name.toLowerCase() === name.toLowerCase()
-      )
-    ) {
+    if (parsedData.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
       console.log(`${name} is already in contacts`);
       return;
     }
@@ -67,10 +57,9 @@ async function addContact(name, email, phone) {
     parsedData.push(newContact);
     await fs.writeFile(contactsPath, JSON.stringify(parsedData));
     console.log(`${name} is added to contacts`);
-  } catch (err) {
-    console.log(err.message);
+  } catch (error) {
+    console.log(error.message);
   }
 }
-
 
 module.exports = { listContacts, getContactById, removeContact, addContact };
